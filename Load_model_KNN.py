3d89ae1c768b60ba90a6b765f7c80050e2f7fd85
thanh <lcthanh.20it9@vku.udn.vn>
2023-06-06 00:59:10 +0700

    Edit file

--- Load_model_KNN.py
+++ Load_model_KNN.py
@@ -1,46 +1,66 @@
-# import các packages cần thiết
 from preprocessing import SimplePreprocessor # Import modul SimplePreprocessor
 from datasets import simpledatasetloader # Import modul simpledatasetloader
 import cv2
 import pickle  #Thư viện này để đọc file model
+from gtts import gTTS
+#import playsound
+import playsound
+import time
 
 # Khởi tạo danh sách nhãn
-classLabels = ["cat", "dog", "panda"]
+classLabels = ["cat", "dog", "duck", "panda"]
+classLabels_TV = ["mèo", "chó", "vịt", "gấu"]
 
-print("[INFO] Đang nạp ảnh để bộ phân lớp dự đoán...")
+print("[Thông Báo] Đang nạp ảnh để bộ phân lớp dự đoán...")
 
 # Thiết lập kích thước ảnh 32 x 32
 sp = SimplePreprocessor(32, 32)
-
 # Tạo bộ nạp dữ liệu ảnh
 sdl = simpledatasetloader.SimpleDatasetLoader(preprocessors=[sp])
 
 # Nạp dữ liệu file ảnh và lưu dưới dạng mảng
-data, _ = sdl.load(["Image\\h2.jpg"])
+data, _ = sdl.load(["Image\\testduck.jpg"])
 
 #Thay đổi cách biểu diễn mảng dữ liệu ảnh
 data = data.reshape((data.shape[0], 3072))
 
 # Nạp model KNN đã train
-print("[INFO] Nạp model k-NN ...")
+print("[Thông Báo] Nạp model k-NN ...")
 model = pickle.load(open('knn.model', 'rb'))
 
 # Dự đoán
-print("[INFO] Thực hiện dự đoán ảnh để phân lớp...")
-preds = model.predict(data) # Trả về danh sách nhãn dự đoán: 0->cat, 1->dog, 2->Panda
+print("[Thông Báo] Thực hiện dự đoán ảnh để phân lớp...")
+
+preds = model.predict(data) # Trả về danh sách nhãn dự đoán: 0->cat, 1->dog, 2->duck
+print(preds)
 
 
-#Đọc file ảnh
-image = cv2.imread("Image\\h2.jpg")
+#lưu âm thanh của tt vào file output.mp3
+text = "Đây là con "  + classLabels_TV[preds[0]]
+output = gTTS(text, lang="vi", slow=False)
+output.save("output.mp3")
+
+# Đọc file ảnh
+image = cv2.imread("Image\\testduck.jpg")
+
+# Thay đổi kích thước ảnh thành 500x500
+resized_image = cv2.resize(image, (500, 500))
+
 # Viết label lên ảnh
-cv2.putText(image, "label: {}".format(classLabels[preds[0]]), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+cv2.putText(resized_image, "label: {}".format(classLabels[preds[0]]), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+
 # Hiển thị ảnh
-cv2.imshow("Image", image)
+cv2.imshow("Image", resized_image)
+
+#if classLabels =panda:
+
+
+
+playsound.playsound('output.mp3', True)# Phát âm thanh label là con vật gì
+# Chờ người dùng nhấn phím bất kỳ để đóng cửa sổ
 cv2.waitKey(0)
 
-
-
-
-
+# Đóng cửa sổ
+cv2.destroyAllWindows()
 
 
--- kNN.py
+++ kNN.py
@@ -1,5 +1,3 @@
-# import các packages cần thiết
-# NeighborsClassifier để triển khai thuật toán k-NN được cung cấp thư viện scikit-learn
 from sklearn.neighbors import KNeighborsClassifier
 # LabelEncoder, một tiện ích để convert nhãn thành số nguyên
 from sklearn.preprocessing import LabelEncoder
@@ -7,6 +5,7 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 # Hàm classification_report để hiển thị kết quả đánh giá hiệu suất của bộ phân loại
 from sklearn.metrics import classification_report
+import search_k
 # Gọi Bộ tiền xử lý dữ liệu (để chuyển kích thước ảnh về 32 x 32)
 from preprocessing import SimplePreprocessor
 # Gọi bộ nạp dữ liệu, khai báo tham số verbose=500 cho biết nạp 500 ảnh để xử lý 1 lần
@@ -15,7 +14,7 @@ from imutils import paths
 import pickle  #Thư viện này để làm việc với file định dạng theo pickle (File model đã huấn luyện)
 
 # Lấy danh sách các ảnh trong folder
-print("[INFO] Nạp ảnh...")
+print("[Thông Báo] Nạp ảnh...")
 imagePaths = list(paths.list_images("datasets/animals"))
 # Khởi tạo bộ tiền xử lý ảnh và nạp ảnh từ foder, thay đổi kích thước ảnh
 sp = SimplePreprocessor(32, 32) # Thiết lập kích thước ảnh 32 x 32
@@ -25,19 +24,21 @@ data = data.reshape((data.shape[0], 3072)) #Thay đổi cách biểu diễn mả
 
 # Hiển thị thông tin bộ nhớ đã dùng
 # Dung lượng = (32x32x3) *3000 ảnh = 3072*3000 ~ 9MB
-print("[INFO] Dung lượng của bộ nhớ chứa dữ liệu ảnh: {:.1f}MB".format(data.nbytes / (1024 * 1000.0)))
+print("[Thông Báo] Dung lượng của bộ nhớ chứa dữ liệu ảnh: {:.1f}MB".format(data.nbytes / (1024 * 1000.0)))
 
 # Chuyển nhãn từ chuỗi sang số nguyễn
 le = LabelEncoder()
 labels = le.fit_transform(labels)
 
-# Chia dữ liều thành 2 phần: 75% để train và 25% để test
-(trainX, testX, trainY, testY) = train_test_split(data, labels,test_size=0.25, random_state=42)
+# Chia dữ liều thành 2 phần: 80% để train và 20% để test
+(trainX, testX, trainY, testY) = train_test_split(data, labels,test_size=0.2, random_state=24)
 
 # Tạo mô hình (Bộ phân loại k-NN)
-print("[INFO] Đánh giá Bộ phân lớp k-NN ...")
-#n_neighbors=1, nghĩa là k = 1
-model = KNeighborsClassifier(n_neighbors=1)
+print("[Thông Báo] Đánh giá Bộ phân lớp k-NN ...")
+
+# k = search_k.search(trainX, trainY, testX,  testY) #23
+k = 23
+model = KNeighborsClassifier(n_neighbors=k)
 
 # Huấn luyện model (bộ phân loại k-NN)
 model.fit(trainX, trainY)
@@ -48,3 +49,9 @@ pickle.dump(model, open("knn.model", 'wb'))
 # Đánh giá hiệu suất Bộ phân lớp k-NN và hiển thị kết quả
 print(classification_report(testY, model.predict(testX),target_names=le.classes_))
 
+# Dự đoán nhãn cho tập dữ liệu test
+predictions = model.predict(testX)
+
+# Tính độ chính xác bằng cách so sánh kết quả dự đoán với nhãn thực tế
+accuracy = (predictions == testY).mean()
+print("Độ chính xác trên tập dữ liệu test: {:.2f}%".format(accuracy * 100))
